811a84369ad345800846b59eaa023dfc60dc6929
Bruna Lekich Louro <brunalekich@gmail.com>
2022-12-08 12:45:06 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+//Chmanado o Canvas:
+
+
+//Tamanho do rover:
+roverWidth = 100;
+roverHeight = 90;
+
+//Imagem de fundo e do rover:
+backgroundImage = "mars.jpg";
+
+//Posição inicial do rover:
+roverX = 10;
+roverY = 10;
+
+//Função pra fazer o background aparecer:
+function uploadBackground() {
+	
+}
+
+//Função pra fazer o rover aparecer:
+function uploadrover() {
+	
+}
+
+function add() {
+	backgroundImgTag =       //definindo uma variável com uma nova imagem
+	backgroundImgTag.onload =         // ajustando uma função, ao carregar essa variável
+	backgroundImgTag.src =        // carregar a imagem
+
+	roverImgTag =            //definindo uma variável com uma nova imagem
+	roverImgTag.onload =      // ajustando uma função, ao carregar essa variável
+	roverImgTag.src =         // carregar a imagem
+}
+
+//Adicinar "escutador de eventos: addEventListener"
+
+
+
+//FUNCTIONS:
+function myKeyDown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("cima");
+		}
+		//COMPLETAR
+}
+
+        
